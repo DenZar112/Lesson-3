798eac360f90b4d93e590f27029f935e73dadc7e
Denis <deza56@gmail.com>
2021-05-13 14:26:15 +0400

    "Lesson 3: Practice_Methods"

--- app/src/main/java/com/zarudin/myapplication_3/MainActivity.java
+++ app/src/main/java/com/zarudin/myapplication_3/MainActivity.java
@@ -1,8 +1,13 @@
 package com.zarudin.myapplication_3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.TaskStackBuilder;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,57 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    } //todo
+
+        String InstanceState;
+        if (savedInstanceState == null){
+            InstanceState = "Первый запуск";
+        }else{
+            InstanceState = "Повторный запуск";
+        }
+
+        Toast.makeText(getApplicationContext(), InstanceState, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+
+
+
+
+
 }
